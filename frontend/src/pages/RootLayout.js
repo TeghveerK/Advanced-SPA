ee40887ca8417a2897a89df1ca885e7d2981a1ac
TeghveerK <tkooner@uci.edu>
2023-02-27 19:46:12 -0800

    Loader Function Relocation

--- frontend/src/pages/RootLayout.js
+++ frontend/src/pages/RootLayout.js
@@ -1,11 +1,16 @@
 import MainNavigation from "../components/MainNavigation";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 
 function RootLayout() {
+  // const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
-      <Outlet />
+      <main>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
     </>
   );
 }
