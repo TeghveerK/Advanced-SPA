52a4d5d18463b2ee7e67607ef73ac16ae4d84247
TeghveerK <tkooner@uci.edu>
2023-03-03 17:37:34 -0800

    Controlling Data to be Deferred

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,11 @@
+import PageContent from "../components/PageContent";
+
 function HomePage() {
-  return <h1>This is the Homepage!</h1>;
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
 }
 
 export default HomePage;
